228b1b58f9f2c051792153b5202c3d3f240890cc
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2022-08-29 18:15:59 +0000

    sm8250-common: parts: Implement HBM & Auto HBM
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: EmanuelCN <emanuelgithub@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: EmanuelCN <emanuelgithub@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,7 +24,7 @@ import android.content.IntentFilter;
 import android.util.Log;
 
 import org.lineageos.settings.thermal.ThermalUtils;
-
+import org.lineageos.settings.utils.FileUtils;
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
@@ -34,5 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
+        FileUtils.enableService(context);
     }
 }
